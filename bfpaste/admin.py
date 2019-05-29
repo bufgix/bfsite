576ee601503b48756ddd607f1acb963617a2aa6d
bufgix <ooruc471@yandex.com>
2019-05-30 00:52:38 +0300

    Added link to admin panel

--- bfpaste/admin.py
+++ bfpaste/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.urls import reverse
 
 from .models import Paste
 
 
 class PasteAdmin(admin.ModelAdmin):
-    list_display = ('author', 'pub_date', 'paste_id')
+    def view_paste(self):
+        return mark_safe(f"<a href='{reverse('bfpaste:show_paste', kwargs={'paste_id': self.paste_id})}'>View</a>")
+    view_paste.short_description = 'Page'
+
+    list_display = ('author', 'pub_date', view_paste)
     readonly_fields = ('paste_id', 'pub_date')
 
 
