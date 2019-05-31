3ad1b186c4c8c1e9d05b9e69286d77530e7ac16d
bufgix <ooruc471@yandex.com>
2019-05-31 22:58:26 +0300

    fix ~ II

--- bfblog/templatetags/markdownify.py
+++ bfblog/templatetags/markdownify.py
@@ -9,7 +9,7 @@ register = template.Library()
 class HighlightRenderer(mistune.Renderer):
     def block_code(self, code, lang=None):
         if not lang:
-            return f"\n<pre><code>{mistune.escape(code)}</code><pre>\n"
+            return f"\n<pre>{mistune.escape(code)}<pre>\n"
         
         lexer = get_lexer_by_name(lang, stripall=True)
         formatter = HtmlFormatter()
--- bfsite/urls.py
+++ bfsite/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+def index(req):
+    return redirect('Blog:index')
 
 urlpatterns = [
-    path('blog/', include('bfblog.urls')),
-    path('paste/', include('bfpaste.urls')),
+    path('', index, name="index"),
+    path('blog/', include('bfblog.urls' , namespace="Blog")),
+    path('paste/', include('bfpaste.urls', namespace="Paste")),
     path('admin/', admin.site.urls),
 ]
 
