8649b52dc56a97f6b33bdfb95c29be58e0de8043
bufgix <ooruc471@yandex.com>
2019-06-03 22:31:23 +0300

    design stuff

--- static/bfblog/js/index.js
+++ static/bfblog/js/index.js
@@ -1,3 +1,45 @@
+// Particles.js
+/*var partJson = {
+    "particles": {
+        "number": {
+            "value": 75
+        },
+        "color":{
+            "value": "#CCCCCC"
+        },
+        "shape":{
+            "type": "circle"
+        },
+        "opacity":{
+            "value": 0.9,
+            "random": true
+        },
+        "size": {
+            "value": 5
+        },
+        "line_linked":{
+            "enable": true,
+            "distance": 120
+        },
+        "move":{
+            "enable": true,
+            "speed": 1,
+            "direction": "top-left"
+        },
+        "interactivity": {
+            "enable": false,
+            "detect_on": "canvas",
+            "events": {
+                "onclick": {
+                    "enable": false,
+                    "mode": "push"
+                }
+            },
+        }
+    } 
+};
+var jsonUri = "data:text/plain;base64,"+window.btoa(JSON.stringify(partJson));*/
+
 function openNav() {
     $(".navbar-toggler").hide("fast")
     document.getElementById("mobilenav").style.width = "100%";
@@ -9,9 +51,17 @@ function closeNav() {
 }
 
 $(document).ready(() => {
-    $('img').wrap(function(){
+    /* Particles json load
+    particlesJS.load('particles-js', jsonUri, () => {
+        console.log('Particles json loaded');
+    })*/
+
+    $(document).find('.post-image').wrap(function(){
         return "<div class='text-center'><a class='venobox_custom' href='" + $(this).attr('src') +"'></a></div>"
     });
+    $(".blog-content").find('img').wrap(function() {
+        return "<div class='text-center'><a class='venobox_custom' href='" + $(this).attr('src') +"'></a></div>"
+    })
 
     $('img').addClass('img-fluid deep');
     $('.venobox_custom').venobox({
